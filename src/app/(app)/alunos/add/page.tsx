54cf3de4b89ddb582c28c5e9b1f7b67e7fa87614
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2025-07-02 17:41:50 -0300

    feat: adicionar a função de criar um novo aluno.
    --Criar o componente AddAlunosForm.tsx
    --Criar a pagina de adicionar alunos
    --Instalar o componente de select do shadcn

--- src/app/(app)/alunos/add/page.tsx
+++ src/app/(app)/alunos/add/page.tsx
@@ -1,10 +1,25 @@
-import Head from "@/components/Head";
-import { X } from "lucide-react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import AddAlunoForm from "@/components/AddAlunosForm";
 
 export default function AddAlunoPage() {
-   return (
-    <div className="p-8">
-      <Head title="Adionando um novo aluno" description="Adicione alunos para sua academia rapidamente" buttonText="Cancelar" buttonVariant={"destructive"} icon={<X></X>}></Head>
-    </div>
-   )
+  const router = useRouter();
+
+  const handleSuccess = () => {
+    router.push("/alunos");
+  };
+
+  const handleCancel = () => {
+    router.back();
+  };
+
+  return (
+      <div className="flex justify-center mt-8">
+        <AddAlunoForm 
+          onSuccess={handleSuccess}
+          onCancel={handleCancel}
+        />
+      </div>
+  );
 }
